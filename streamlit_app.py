25068aedbb285cbea39569f8252305a2b2003ae4
Agung Setiaji <53251171+cracktlor@users.noreply.github.com>
2023-08-07 11:41:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
 import streamlit
 
 streamlit.title('My Parents New  Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Outmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlitt.text('Hard-Boiled Free-Range Egg')
